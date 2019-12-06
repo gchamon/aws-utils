06dae54a0f0546398199c5307bd7c508610f835d
Gabriel Chamon Araujo <gchamon@live.com>
2019-12-06 15:49:54 -0300

    exposed timeout; added ConnectionError handling

--- aws_utils/session.py
+++ aws_utils/session.py
@@ -4,21 +4,23 @@ import requests
 METADATA_URL = "http://169.254.169.254/latest/meta-data"
 
 
-def get_session_from_attached_role(region_name):
-    role_name = get_role_attached_to_instance()
+def get_session_from_attached_role(region_name, timeout=1):
+    role_name = get_role_attached_to_instance(timeout=timeout)
     if role_name:
         return get_session_from_role(role_name=role_name,
-                                     region_name=region_name)
+                                     region_name=region_name,
+                                     timeout=timeout)
     else:
         return boto3
 
 
-def get_role_attached_to_instance():
+def get_role_attached_to_instance(timeout=1):
     try:
-        response = requests.get(f"{METADATA_URL}/iam/security-credentials")
+        response = requests.get(f"{METADATA_URL}/iam/security-credentials",
+                                timeout=timeout)
         response.raise_for_status()
         return response.text
-    except requests.HTTPError:
+    except (requests.HTTPError, requests.exceptions.ConnectionError):
         return None
 
 
@@ -33,5 +35,5 @@ def get_session_from_role(role_name, region_name, timeout=1):
                              aws_access_key_id=credentials["AccessKeyId"],
                              aws_secret_access_key=credentials["SecretAccessKey"],
                              aws_session_token=credentials["Token"])
-    except requests.HTTPError:
+    except (requests.HTTPError, requests.exceptions.ConnectionError):
         return boto3.Session(region_name=region_name)
